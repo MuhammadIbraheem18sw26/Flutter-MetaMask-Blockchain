0824d94576ea27ac46f9baa296024e3082a98e02
Muhammad Ibraheem <ibrahimjhullan7@gmail.com>
2022-03-13 01:00:51 +0500

    Compile NFT contract successfully

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
-  solidity: "0.8.4",
+   solidity: "0.8.4",
+    defaultNetwork: "rinkeby",
+     networks: {
+       hardhat: {
+       },
+       rinkeby: {
+       url: API_URL,
+       accounts: [`0x${PRIVATE_KEY}`]
+       }
+     },
 };
+
+
